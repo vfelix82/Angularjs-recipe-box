b5fedab1c89616eb3fe30b1493cbdd70ed8d450a
faiza husain and victor felix <faiza@>
2018-03-27 10:29:07 -0700

    basic menu with just items and no ingredients or directions.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Menu } from './models/menu.models';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'Faiza and Victor\'s Bakery';
+  sweetItems: Menu[] = [
+    new Menu("Challa"),
+    new Menu("Coffee and Cinnamon Croissant"),
+    new Menu("Blueberry Scone"),
+    new Menu("Lemon and Cheesecake Pastry"),
+    new Menu("Chocolate Pastry")
+  ];
+
+  savouryItems: Menu[] = [
+    new Menu("Spinach, Artichoke, & Chicken Puff Pastry Parcels"),
+    new Menu("Vegetable Pastries"),
+    new Menu("Flourless Plantain Empanadas"),
+    new Menu("Feta and Sun Dried Tomato Pesto Swirls"),
+    new Menu("Roasted Brocccoli, Potato, and Sausage Tart")
+  ];
+
+  cookieItems: Menu[] = [
+    new Menu("Chococlate Chip Cookie"),
+    new Menu("Macaroons"),
+    new Menu("Peanut Butter Cookies"),
+    new Menu("Butter Pecan Cookies"),
+    new Menu("Snickerdoodle")
+  ];
+
+  cakeItems: Menu[] = [
+    new Menu("Angel Food Cake"),
+    new Menu("Red Velvet Cake"),
+    new Menu("Original Cheese Cake"),
+    new Menu("Molten Chocolate Cake"),
+    new Menu("Coffee Cake")
+  ];
+
+  coffeeItems: Menu[] = [
+    new Menu("Mocha Latte"),
+    new Menu("Americano"),
+    new Menu("Caffe Latte"),
+    new Menu("Espresso"),
+    new Menu("Coffee Cake")
+  ];
 }
